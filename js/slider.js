b9e559bcfde6c10255133685132f140305dbc987
davidvarley <thedavidvarley@gmail.com>
2015-02-27 09:57:44 +0000

    first commit

---
+++ js/slider.js
@@ -0,0 +1,25 @@
+function slider () {
+
+	$(".slider #1").show("fade", 500);
+	$(".slider #1").delay(5500).hide("slide", {direction: 'up' }, 500)
+
+	var sc = $(".slider img").size();
+	var count = 2;
+
+	setInterval(function(){
+
+		$(".slider #"+count).show("slide", {direction: 'down'}, 500);
+		$(".slider #"+count).delay(5500).hide("slide", {direction: 'up'}, 500);
+
+		if(count == sc){
+
+			count=1;
+
+		}else{ 
+
+			count = count + 1;
+		}
+	}, 6500);
+
+
+}
